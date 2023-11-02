7dfabc2b5ef17a8d0318e86741510eb1aac3343e
blackalle <145697709+blackalle@users.noreply.github.com>
2023-11-02 18:14:59 +0000

    nueva funcion mis_funciones.py

--- mis_funciones.py
+++ mis_funciones.py
@@ -4,7 +4,7 @@ from nltk.corpus import stopwords
 wpt = WordPunctTokenizer()
 stop_words = set(stopwords.words('spanish'))
 
-def normalize_document(doc):
+def normalizar_doc(doc):
     doc = doc.replace("!", "").replace("¡", "").replace(",", "").replace(".", "").replace(";", "")
     doc = doc.lower()
     doc = doc.strip()
@@ -15,3 +15,23 @@ def normalize_document(doc):
     # re-create document from filtered tokens
     doc = ' '.join(filtered_tokens)
     return doc
+
+# Path: mis_funciones.py
+from nltk.tokenize import WordPunctTokenizer
+from nltk.corpus import stopwords
+
+wpt = WordPunctTokenizer()
+stop_words_eng = set(stopwords.words('english'))
+
+def normalize_doc(doc):
+    doc = doc.replace("!", "").replace("¡", "").replace(",", "").replace(".", "").replace(";", "")
+    doc = doc.lower()
+    doc = doc.strip()
+    # tokenize document
+    tokens = wpt.tokenize(doc)
+    # filter stopwords out of document
+    filtered_tokens = [token for token in tokens if token not in stop_words_eng]
+    # re-create document from filtered tokens
+    doc = ' '.join(filtered_tokens)
+    return doc
+
